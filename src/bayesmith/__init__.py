a649261d2edcd9be82b25cf120a1af441e3cb359
Zheng Zhang <zheng_zhang1@alumni.brown.edu>
2026-08-23 00:49:30 +0100

    feat: error family and package skeleton

---
+++ src/bayesmith/__init__.py
@@ -0,0 +1,5 @@
+"""bayesmith: a graph of operators is a Bayesian model."""
+
+from bayesmith.errors import BayesmithError, GraphError, TraceError
+
+__all__ = ["BayesmithError", "GraphError", "TraceError"]
